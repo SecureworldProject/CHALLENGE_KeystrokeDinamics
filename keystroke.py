12f7aaaa991a8a23b8d99796f71dea1c701e272f
goomeez21 <122268946+goomeez21@users.noreply.github.com>
2024-01-29 19:15:31 +0100

    Update keystroke.py

--- keystroke.py
+++ keystroke.py
@@ -54,9 +54,9 @@ def executeChallenge():
     if props_dict['metodo']=='parental':
         if cad>7:
             #si la categoria es 8 o 9 es el padre o madre por lo tanto devolvera un 1
-            cad='\0'
+            cad=0
         else:
-            cad='\u0001'
+            cad=1
             
     #y generamos el resultado
     cad="%d"%(cad)
